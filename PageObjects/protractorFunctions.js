0457529c2a271b84b7436d401d6ad80469ee3445
Hassan Khan <hasankhn06@gmail.com>
2017-01-16 14:34:14 +0500

    adds new files

--- PageObjects/protractorFunctions.js
+++ PageObjects/protractorFunctions.js
@@ -1,18 +1,14 @@
 
 module.exports = function () {
 
-    this.start = function (url) {
-        beforeEach(function() {
-            return browser.ignoreSynchronization = true;
-        });
-        browser.get(url)
-        // expect(browser.getCurrentUrl()).toBe(url);
-        // browser.driver.get('http://api.caremerge.loc/hfp-web');
-
-        // browser.driver.findElement(By.id('username')).sendKeys('superuser1');
-        // browser.driver.findElement(By.id('password')).sendKeys('123');
-        // browser.driver.findElement(By.id('login_submit')).click();
-
+    var driver = browser.driver;
+    this.start = function (url,username,password,login_btn,nextPage) {
+        driver.get(url);
+        driver.findElement(by.id('username')).sendKeys(username);
+        driver.findElement(by.id('password')).sendKeys(password);
+        driver.findElement(by.id(login_btn)).click();
+        expect(driver.getTitle()).toEqual(nextPage)
+      
     }
 
 }
--- apps/geturl.js
+++ apps/geturl.js
@@ -1,10 +1,8 @@
+var createPage = require("../PageObjects/protractorFunctions.js");
+var helper = new createPage();
 describe('Protractor Demo App', function () {
     it('should add one and two', function () {
-        browser.driver.get('http://caremerge.loc/hfp-web/');
-        browser.driver.findElement(by.id('username')).sendKeys("superuser1");
-        browser.driver.findElement(by.id('password')).sendKeys("123");
-        browser.driver.findElement(by.id('login_submit')).click();
-        expect(browser.driver.getTitle()).toEqual('My Dashboard')
+       helper.start("http://caremerge.loc/hfp-web",'superuser1','123','login_submit','My Dashboard');
 
     });
 });
